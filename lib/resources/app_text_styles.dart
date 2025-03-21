1b2563d469dc17e902edb4f75f8798457e2d14f9
Abhay Kumar <abhay.kumar@logiclyinfotech.com>
2025-03-21 12:48:52 +0530

    code tested

--- lib/resources/app_text_styles.dart
+++ lib/resources/app_text_styles.dart
@@ -4,92 +4,79 @@ import 'app_colors.dart';
 
 class AppTextStyles {
   static TextStyle appBarTitleStyle() {
-    return  TextStyle(
+    return const TextStyle(
       color: AppColors.scoButtonColor,
       fontSize: 18,
       fontWeight: FontWeight.bold,
-      
     );
   }
 
   static TextStyle subTitleTextStyle() {
-    return  TextStyle(
+    return const TextStyle(
       color: Color(0xff8491A8),
       fontSize: 12,
-        
-
     );
   }
 
   static TextStyle titleTextStyle() {
-    return  TextStyle(
+    return const TextStyle(
       color: AppColors.scoButtonColor,
       fontSize: 14,
-      fontWeight: FontWeight.w600,      
-
+      fontWeight: FontWeight.w600,
     );
   }
 
   static TextStyle titleBoldTextStyle() {
-    return  TextStyle(
+    return const TextStyle(
       color: AppColors.scoButtonColor,
       fontSize: 16,
-      fontWeight: FontWeight.bold,      
-
+      fontWeight: FontWeight.bold,
     );
   }
 
   static TextStyle titleBoldThemeColorTextStyle() {
-    return  TextStyle(
+    return const TextStyle(
       color: Color(0xffCDB383),
       fontSize: 16,
-      fontWeight: FontWeight.bold,      
-
+      fontWeight: FontWeight.bold,
     );
   }
-  static TextStyle normalThemeColorTextStyle() {
-    return  TextStyle(
-      color: Color(0xffCDB383),
-      fontSize: 14,      
 
+  static TextStyle normalThemeColorTextStyle() {
+    return const TextStyle(
+      color: Color(0xffCDB383),
+      fontSize: 14,
     );
   }
 
   static TextStyle normalTextStyle() {
-    return  TextStyle(
+    return const TextStyle(
       color: Colors.black,
-      fontSize: 14,      
-
+      fontSize: 14,
     );
   }
 
   static TextStyle bold15ScoButtonColorTextStyle() {
-    return  TextStyle(
+    return const TextStyle(
       color: AppColors.scoButtonColor,
       fontWeight: FontWeight.bold,
       fontSize: 15,
-      height: 1.5,      
-
+      height: 1.5,
     );
   }
+
   static TextStyle myApplicationsEditButton() {
-    return  TextStyle(
+    return const TextStyle(
       color: Colors.white,
       fontSize: 10,
-      fontWeight: FontWeight.w500,      
-
+      fontWeight: FontWeight.w500,
     );
   }
+
   static TextStyle drawerButtonsStyle() {
-    return  TextStyle(
-        color: Colors.white,
-        fontSize: 14,      
+    return const TextStyle(
+      color: Colors.white,
+      fontSize: 14,
     );
   }
-
-
-
-
-
-
 }
